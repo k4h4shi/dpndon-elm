0edb76f4f9d2a4a2a92add1d11a62c282be1e23c
kotaro takahashi <kotaro.t@k4h4shi.com>
2019-12-30 18:02:55 +0900

    add pages

--- src/main.elm
+++ src/main.elm
@@ -2,10 +2,11 @@ module Main exposing (Model, Msg(..), init, main, update, view)
 
 import Browser exposing (Document)
 import Browser.Navigation as Nav
-import Html exposing (Html, a, button, div, footer, h1, h2, input, nav, span, text)
-import Html.Attributes exposing (class, href, value)
+import Html exposing (Attribute, Html, a, article, button, div, footer, h1, h2, input, nav, span, text)
+import Html.Attributes as Attr exposing (class, disabled, href, value)
 import Html.Events exposing (onClick, onInput)
-import Url
+import Url exposing (Url)
+import Url.Parser as Parser exposing ((</>), Parser, oneOf, s, string)
 
 
 
@@ -28,16 +29,37 @@ main =
 -- MODEL
 
 
+type alias Dependency =
+    { name : String
+    , version : String
+    , description : String
+    , homepage : String
+    }
+
+
 type alias Model =
     { key : Nav.Key
     , url : Url.Url
-    , input : String
+
+    -- Top
+    , packageName : String
+    , dependencies : List Dependency
     }
 
 
 init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
 init _ url key =
-    ( { url = url, key = key, input = "" }, Cmd.none )
+    ( { url = url
+      , key = key
+      , packageName = ""
+      , dependencies =
+            [ Dependency "babel-cli" "6.26.0" "Babel command line." "https://babeljs.io/"
+            , Dependency "babel-preset-es2015" "6.24.1" "Babel preset for all es2015 plugins." "https://babeljs.io/"
+            , Dependency "chai" "4.2.0" "BDD/TDD assertion library for node.js and the browser. Test framework agnostic." "https://www.chaijs.com/"
+            ]
+      }
+    , Cmd.none
+    )
 
 
 
@@ -47,8 +69,9 @@ init _ url key =
 type Msg
     = LinkClicked Browser.UrlRequest
     | UrlChanged Url.Url
+      -- Top
     | Input String
-    | Reset
+    | Submit
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -69,11 +92,22 @@ update msg model =
             , Cmd.none
             )
 
+        -- Top
         Input value ->
-            ( { model | input = value }, Cmd.none )
+            ( { model | packageName = value }, Cmd.none )
 
-        Reset ->
-            ( { model | input = "" }, Cmd.none )
+        Submit ->
+            let
+                { url } =
+                    model
+
+                path =
+                    "/packages/" ++ model.packageName
+
+                nextUrl =
+                    Url.toString { url | path = path }
+            in
+            ( { model | packageName = "" }, Nav.pushUrl model.key nextUrl )
 
 
 
@@ -86,20 +120,123 @@ subscriptions _ =
 
 
 
+-- ROUTING
+
+
+type Route
+    = TopRoute
+    | NpmPackageRoute String
+
+
+parser : Parser (Route -> a) a
+parser =
+    oneOf
+        [ Parser.map TopRoute Parser.top
+        , Parser.map NpmPackageRoute (s "packages" </> string)
+        ]
+
+
+
 -- VIEW
 
 
 view : Model -> Document Msg
 view model =
+    let
+        route =
+            Parser.parse parser model.url
+    in
+    case
+        route
+    of
+        Nothing ->
+            viewNotFoundPage model
+
+        Just TopRoute ->
+            viewTopPage model
+
+        Just (NpmPackageRoute packageName) ->
+            viewNpmPackagePage model packageName
+
+
+
+--- VIEW PAGES
+
+
+viewTopPage : Model -> Document Msg
+viewTopPage model =
+    let
+        { packageName } =
+            model
+
+        submitButtonDisabled =
+            packageName == ""
+    in
     { title = "Dpndon"
     , body =
         [ div [ class "hero is-fullheight " ]
             [ viewHeader
-            , viewMain model
+            , div [ class "hero-body" ]
+                [ div [ class "container has-text-centered" ]
+                    [ h1 [ class "title" ] [ text "Dpndon" ]
+                    , h2 [ class "subtitle" ] [ text "List Homepages of npm modules that the project depend on." ]
+                    , div [ class "field has-addons has-addons-centered " ]
+                        [ div [ class "control" ]
+                            [ input [ class "input", value packageName, onInput Input ] []
+                            ]
+                        , div [ class "control" ] [ button [ class "button is-primary", onClick Submit, disabled submitButtonDisabled ] [ text "Go" ] ]
+                        ]
+                    ]
+                ]
             , viewFooter
             ]
         ]
     }
+
+
+viewNpmPackagePage : Model -> String -> Document Msg
+viewNpmPackagePage model packageName =
+    let
+        { dependencies } =
+            model
+    in
+    { title = "Dpndon - " ++ packageName
+    , body =
+        [ div [ class "hero is-fullheight " ]
+            [ viewHeader
+            , div [ class "hero-body" ]
+                [ div [ class "container has-text-centered" ]
+                    [ h1 [ class "title" ] [ text packageName ]
+
+                    -- TODO: 依存性のリストを表示する
+                    ]
+                ]
+            , viewFooter
+            ]
+        ]
+    }
+
+
+viewNotFoundPage : Model -> Document Msg
+viewNotFoundPage model =
+    { title = "Dpndon - Notfound"
+    , body =
+        [ div [ class "hero is-fullheight " ]
+            [ viewHeader
+            , div [ class "hero-body" ]
+                [ div [ class "container has-text-centered" ]
+                    [ h1 [ class "title" ] [ text "404" ]
+                    , h2 [ class "subtitle" ] [ text "Sorry, the page not found" ]
+                    ]
+                ]
+            , viewFooter
+            ]
+        ]
+    }
+
+
+
+--- VIEW COMMONS
 
 
 viewHeader : Html Msg
@@ -112,27 +249,6 @@ viewHeader =
                     ]
                 ]
             ]
-        ]
-
-
-viewMain : Model -> Html Msg
-viewMain model =
-    div [ class "hero-body" ]
-        [ div [ class "container has-text-centered" ]
-            [ h1 [ class "title" ] [ text "Dpndon" ]
-            , h2 [ class "subtitle" ] [ text "List Homepages of npm modules that the project depend on." ]
-            , viewMainForm model
-            ]
-        ]
-
-
-viewMainForm : Model -> Html Msg
-viewMainForm model =
-    div [ class "field has-addons has-addons-centered " ]
-        [ div [ class "control" ]
-            [ input [ class "input", value model.input, onInput Input ] []
-            ]
-        , div [ class "control" ] [ button [ class "button is-primary", onClick Reset ] [ text "Go" ] ]
         ]
 
 
